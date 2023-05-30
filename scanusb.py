30fd7abbea878712bf47daf57ddc4f5e17a1022a
Daniele Rizzieri <daniele96rizzieri@gmail.com>
2023-05-30 16:36:38 +0200

    ported to Linux, adapted win side

--- scanusb.py
+++ scanusb.py
@@ -1,4 +1,4 @@
-import subprocess, platform
+import subprocess, platform, shlex
 
 def win_usbscan():
     device_scan_command = "powershell pnputil /enum-devices /connected  /class \"Ports\"".split()
@@ -40,26 +40,80 @@ def win_usbscan():
     print(f"| {f'FTDI devices: {FTDI_count}'.rjust(dev_str_size+port_str_size+1)} |")
     print(f"+{'-'*(dev_str_size+1)}{'-'*(port_str_size+2)}+")
 
-def linux_usbscan():
-    device_scan_command = 
-    device_scan_script = ''' 
-    for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-    (
-        syspath="${sysdevpath%/dev}"
-        devname="$(udevadm info -q name -p $syspath)"
-        [[ "$devname" == "bus/"* ]] && exit
-        eval "$(udevadm info -q property --export -p $syspath)"
-        [[ -z "$ID_SERIAL" ]] && exit
-        echo "$ID_SERIAL_SHORT,/dev/$devname,0x$ID_VENDOR_ID,$ID_USB_DRIVER,$ID_SERIAL"
-    )
-    done''' # Reference: https://gist.github.com/burgeon-env/b7ab4eda18fa97a7e0ebd9633b015267
-    dev_scan_proc = subprocess.Popen(device_scan_command, stdout=subprocess.PIPE,)
+def win_usbscan_():
+    dev_l = []
+    device_scan_command = "powershell pnputil /enum-devices /connected  /class \"Ports\"".split()
+    dev_scan_proc = subprocess.Popen(device_scan_command, shell=True, stdout=subprocess.PIPE,)
     dev_scan_raw_output = dev_scan_proc.stdout.read().decode().replace("\r\n", "\n")
-    pass
+    dev_str_l = dev_scan_raw_output.split("\n\n")[1:-1]
+
+    for n,dev_str in enumerate(dev_str_l):
+        field_str_l = dev_str.splitlines()
+        for field_str in field_str_l:
+            if "Instance ID:" in field_str:
+                dev_name=field_str.split(":")[-1].split("+")[-1].strip().split("\\0")[0]
+                #print(f"ID --> {dev_name}")
+                continue 
+
+            if "Device Description:" in field_str:
+                com_port=field_str.strip().split(":")[-1].split("(")[-1][:-1] 
+                #print(f"COM --> {com_port}")
+                continue
+
+            if "Manufacturer" in field_str:
+                manufacturer=field_str.strip().split(":")[-1]
+                #print(f"Manufacturer --> {manufacturer}")
+                if "FTDI" not in manufacturer and "ftdi" not in manufacturer:
+                    break
+                else:
+                    dev_l.append((dev_name,com_port))
+                    print("|"+dev_name.rjust(dev_str_size)+" | "+com_port.ljust(port_str_size)+"|")
+    return dev_l
+
+def print_dev_l(dev_l, dev_str_size=20, port_str_size=6):
+    tab_line = f"+{'-'*(dev_str_size+1)}+{'-'*(port_str_size+1)}+"
+    bottom_line = f"+{'-'*(dev_str_size+1)}{'-'*(port_str_size+2)}+"
+
+    print(tab_line)
+    print("|"+f"DEVICE ({len(dev_l)}) ".rjust(dev_str_size)+" | "+"PORT".ljust(port_str_size)+"|")
+    print(tab_line)
+
+    for (dev,port) in dev_l:
+        print("|"+dev.rjust(dev_str_size)+" | "+port.ljust(port_str_size)+"|")
+    print(bottom_line)
+
+
+def linux_usbscan():
+    dev_l = []
+    # Reference: https://gist.github.com/burgeon-env/b7ab4eda18fa97a7e0ebd9633b015267
+    device_scan_script = """/bin/bash -c 
+    'for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}";
+        devname="$(udevadm info -q name -p "$syspath")";
+        [[ "$devname" == "bus/"* ]] && exit;
+        eval "$(udevadm info -q property --export -p "$syspath")";
+        [[ -z "$ID_SERIAL" ]] && exit;
+        echo "$ID_SERIAL_SHORT,/dev/$devname,0x$ID_VENDOR_ID,$ID_USB_DRIVER,$ID_SERIAL";
+    )
+    done'
+    """
+    device_scan_command = shlex.split(" ".join(" ".join(device_scan_script.splitlines()).split()))
+    dev_scan_proc = subprocess.Popen(device_scan_command, stdout=subprocess.PIPE)
+    for line in dev_scan_proc.stdout.read().decode().splitlines():
+        if any(x in line for x in ["ftdi", "FTDI", '0x0403']):
+            #print(line)
+            dev_specs = line.split(',')
+            dev_name = f"{dev_specs[4]} ({dev_specs[0]})"
+            com_port = dev_specs[1]
+            dev_l.append((dev_name,com_port))
+    return dev_l
 
 if __name__ == '__main__':
 	#print(f"System is {platform.system()}")
     if platform.system() == "Windows":
-        win_usbscan()
+        dev_list = win_usbscan()
+        print_dev_l(dev_list, dev_str_size=20, port_str_size=6)
     elif platform.system() == "Linux":
-        linux_usbscan()
+        dev_list = linux_usbscan()
+        print_dev_l(dev_list, dev_str_size=31, port_str_size=14)
